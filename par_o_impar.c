f849fcdcac5e5274d2ecfc3223b3a02bf2b91392
GiovanniZavala <98335196+GiovanniZavala@users.noreply.github.com>
2022-03-07 07:59:49 -0600

    Add files via upload

---
+++ par_o_impar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdib.h>
+
+printf ("introduce number")
+int number = atoi(argv[1]);
+
+if a % 2 == 0:
+    print('El número', a, 'es par.')
+else:
+    print('El número', a, 'es impar.')
