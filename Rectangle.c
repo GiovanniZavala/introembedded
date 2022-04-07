6229b2cf415be78a820fc02a2d2dda2f046a02a7
GiovanniZavala <98335196+GiovanniZavala@users.noreply.github.com>
2022-04-06 23:36:26 -0500

    Add files via upload

---
+++ Rectangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int height;
+int width;
+
+int main () { 
+
+    printf("enter height: \n");
+    scanf(" %d", height);
+    printf("enter width: \n");
+    scanf("%d", width);
+
+    area = height*width;
+    printf("area of rectangle = %d cm\n", area);
+
+    if (height = width){
+        if (height = width)
+        printf("%d is a square", square);
+        else 
+        printf("%d is a rectangle", rectangle);
+    }
+return 0;
+    
+}
